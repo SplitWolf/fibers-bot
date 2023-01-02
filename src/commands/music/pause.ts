cab9c9c7de5e6e310cc97ac8cb123e84ab13d5bc
SplitWolf <mat.jake1360@gmail.com>
2023-01-02 00:04:11 -0500

    feat: Placeholder files for music commands

---
+++ src/commands/music/pause.ts
@@ -0,0 +1,20 @@
+/*import {
+	AudioPlayerStatus,
+	AudioResource,
+	entersState,
+	joinVoiceChannel,
+	VoiceConnectionStatus,
+} from '@discordjs/voice';
+import { Command } from "../base";
+
+
+
+class pause extends Command {
+    if (subscription) {
+        subscription.audioPlayer.pause();
+        await interaction.reply({ content: `Paused!`, ephemeral: true });
+    } else {
+        await interaction.reply('Not playing in this server!');
+    }
+    
+}*/
