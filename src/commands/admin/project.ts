59bc2da1805df4cc6da6c5f20793f72e3b6f117b
SplitWolf <mat.jake1360@gmail.com>
2023-12-07 18:58:31 -0500

    feat(commands): Add project command for develop projects
    Creates a set of channels, a role and a category based on the project

---
+++ src/commands/admin/project.ts
@@ -0,0 +1,182 @@
+import { CategoryChannel, ChannelType, ChatInputCommandInteraction, Client, CommandInteraction, Guild, InteractionResponse, OverwriteType, PermissionFlagsBits, Role, SlashCommandBuilder, User } from "discord.js";
+import { Command } from "../base";
+
+
+export class project extends Command {
+    constructor() {
+        const data = new SlashCommandBuilder()
+        .setName('project')
+        .setDescription('Sets up channels for development projects.')
+        .addSubcommand(createSub =>  createSub
+            .setName('create')
+            .setDescription('Creates the channels')
+            .addStringOption(option => option.setName('project_name').setDescription('The name of the project').setRequired(true))
+            .addRoleOption(option => option.setName('project_role').setDescription('The role for users of the project').setRequired(false))
+        )
+        .addSubcommand(deleteSub =>
+            deleteSub
+            .setName('remove')
+            .setDescription('Removes the channels')
+            .addStringOption(option => option.setName('project_name').setDescription('The name of the project').setRequired(true))
+        )
+
+        super(data)
+    }
+    async execute(client: Client, interaction: CommandInteraction): Promise<InteractionResponse<boolean>> {
+        if(!interaction.isChatInputCommand) return;
+        const interactionCmd = interaction as ChatInputCommandInteraction;
+        //@ts-ignore
+        switch(interactionCmd.options.getSubcommand()) {
+            case 'create':
+                this.createChannels(interactionCmd);
+                break;
+            case 'remove':
+                this.deleteChannels(interactionCmd);
+                break;
+        }
+        
+       //interaction.deferReply();
+    }
+    //TODO: Permission checks
+    //TODO: Delete custom role
+
+
+    async createChannels(interaction: ChatInputCommandInteraction) {
+        const name = interaction.options.getString('project_name');
+        const role = interaction.options.getRole('project_role');
+        const guild = interaction.guild;
+    
+        
+        if(role == undefined) {
+            guild.roles.fetch().then(roles => {
+                let existingRole = roles.find(role => role.name == name)
+                if(existingRole != undefined) {
+                    console.log('Role found: '+ existingRole.name + ": " + existingRole.id)
+                    this.buildChannels(guild,name,existingRole);
+                } else {
+                    guild.roles.create({name: name}).then(role => this.buildChannels(guild, name, role))
+                }
+            })
+        } else {
+            this.buildChannels(guild,name,role as Role);
+        }
+        
+        interaction.reply({content: `Setting up channels for project: ${name}`, ephemeral: true});
+    }
+    
+
+    async buildChannels(guild: Guild, name: string,  role: Role) {
+        let everyonePerms =  {
+            id: guild.roles.everyone.id,
+            deny: [
+                PermissionFlagsBits.ViewChannel, 
+                PermissionFlagsBits.SendMessages, 
+                PermissionFlagsBits.SendMessagesInThreads,
+                PermissionFlagsBits.CreatePublicThreads,
+                PermissionFlagsBits.CreatePrivateThreads,
+                PermissionFlagsBits.MentionEveryone,
+                PermissionFlagsBits.UseEmbeddedActivities,     
+                PermissionFlagsBits.Connect,
+                PermissionFlagsBits.Speak,
+                PermissionFlagsBits.Stream,
+            ]
+        };
+        guild.channels.create({name: name, type: ChannelType.GuildCategory, permissionOverwrites: [
+            everyonePerms,
+            {
+                id: role.id,
+                allow: [
+                    PermissionFlagsBits.ViewChannel
+                ],
+                type: OverwriteType.Role
+            }
+        ]}).then(category => {
+            category.children.create({name: 'project-info', type: ChannelType.GuildText, permissionOverwrites: [
+                everyonePerms,
+                {
+                    id: role.id,
+                    allow: [
+                        PermissionFlagsBits.ViewChannel,
+                        PermissionFlagsBits.SendMessagesInThreads,
+                    ],
+                    type: OverwriteType.Role
+                }
+            ]});
+            category.children.create({name: 'project-discussion', type: ChannelType.GuildText, permissionOverwrites: [
+                everyonePerms,
+                {
+                    id: role.id,
+                    allow: [
+                        PermissionFlagsBits.ViewChannel,
+                        PermissionFlagsBits.SendMessages,
+                        PermissionFlagsBits.SendMessagesInThreads,
+                        PermissionFlagsBits.CreatePublicThreads,
+                        PermissionFlagsBits.MentionEveryone,
+                    ],
+                    type: OverwriteType.Role
+                }
+            ]});
+            category.children.create({name: 'git-updates', type: ChannelType.GuildText, permissionOverwrites: [
+                everyonePerms,
+                {
+                    id: role.id,
+                    allow: [
+                        PermissionFlagsBits.ViewChannel,
+                        PermissionFlagsBits.SendMessagesInThreads,
+                        PermissionFlagsBits.CreatePublicThreads,
+                    ],
+                    type: OverwriteType.Role
+                }
+            ]});
+            category.children.create({name: 'Project Talk', type: ChannelType.GuildVoice, permissionOverwrites: [
+                everyonePerms,
+                {
+                    id: role.id,
+                    allow: [
+                        PermissionFlagsBits.ViewChannel,
+                        PermissionFlagsBits.SendMessages,
+                        PermissionFlagsBits.SendMessagesInThreads,
+                        PermissionFlagsBits.CreatePublicThreads,
+                        PermissionFlagsBits.MentionEveryone,
+                        PermissionFlagsBits.Connect,
+                        PermissionFlagsBits.Speak,
+                        PermissionFlagsBits.Stream
+                    ],
+                    type: OverwriteType.Role
+                }
+            ]})
+        })
+    }
+
+    async deleteChannels(interaction: ChatInputCommandInteraction) {
+        const name = interaction.options.getString('project_name');
+        const guild = interaction.guild;
+
+        guild.roles.fetch().then(roles => {
+            let role = roles.find(role => role.name == name)
+            if(role != undefined) {
+                role.delete();
+            }
+        })
+
+        guild.channels.fetch().then(channels => {
+            let namedChannels = channels.filter(channel =>  channel.name == name);
+                //console.log(channel.name + ": " + channel.id + "\nName: " + name + "\nDebug info: " + (channel.name == name));
+            let category: CategoryChannel;
+            namedChannels.forEach(channel => {
+                if(channel.type == ChannelType.GuildCategory) {
+                    category = channel;
+                }
+            });
+            if(category != undefined) {
+                let toDelete = channels.filter(channel => channel.parentId == category.id)
+                toDelete.forEach(channel => channel.delete());
+                category.delete();
+            }
+            
+        })
+
+        interaction.reply({content: `Removed project: ${name}`, ephemeral: true});
+    }
+}
+
