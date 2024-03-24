a75438f8549678ad681f9592322938b63b0868c2
SplitWolf <mat.jake1360@gmail.com>
2024-03-23 22:01:24 -0400

    refactor: Move load cmds to function, Change command type.

--- src/client.ts
+++ src/client.ts
@@ -1,50 +1,33 @@
-import { Client, GatewayIntentBits, Partials, Interaction, Collection, ActivityType } from 'discord.js';
-import * as glob from 'glob';
+import { Client, GatewayIntentBits, Interaction, Collection, ActivityType, InteractionType } from 'discord.js';
+import glob  from 'glob';
 import { Command } from './commands/base';
 require('dotenv').config();
-
-//TODO: FIX DISCORD.JS VOICE IMPELEMENTATION
 
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
-const commands = new Collection<String, Command>();
+let commands = new Collection<String, Command>();
 let commandFiles: string[];
 // Files to exculde from the command search because they are not commands
-const excludes: string[] = ["base"];
+const excludes: string[] = ["base", "ratwisdom", "hypixel"];
 
-glob(__dirname + "/commands/**/*", (err, res) => {
-	if (err) {
-		console.log(err)
-	} else {
-		commandFiles = res.filter(file => file.endsWith('.ts'));
-		commandFiles = commandFiles.filter((file) => !excludes.some((exclude) => file.includes(exclude)));
-		(async () => {
-			for (const file of commandFiles) {
-				const cmdImp = await import(file)
-				if (Object.keys(cmdImp).length !== 0) {
-					const cmd: Command = new cmdImp[Object.keys(cmdImp)[0].toString()];
-					commands.set(cmd.data.name, cmd)
-				}
-			}
-		})();
-	}
-})
+loadCommands();
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 
 	console.log("-----------------------------------------------------------");
 	console.log(
-		`Logged in as ${client.user.username}#${client.user.discriminator} running version 2.1.0`
+		`Logged in as ${client.user!.username}#${client.user!.discriminator} running version 2.1.0`
 	);
-	client.user.setActivity({ name: "/help", type: ActivityType.Listening });
-	console.log(`${client.user.username} is on ${client.guilds.cache.size} server(s)!`);
+	client.user!.setActivity({ name: "/help", type: ActivityType.Listening });
+	console.log(`${client.user!.username} is on ${client.guilds.cache.size} server(s)!`);
 	console.log("-----------------------------------------------------------");
 });
 
 client.on("interactionCreate", async (interaction: Interaction) => {
-	if (!interaction.isCommand()) return;
+	if (interaction.type !== InteractionType.ApplicationCommand) return;
+	if (!interaction.isChatInputCommand()) return;
 
 	const command = commands.get(interaction.commandName);
 
@@ -63,6 +46,31 @@ client.on("interactionCreate", async (interaction: Interaction) => {
 	}
 })
 
+function loadCommands() {
+	console.log('Loading commands...')
+	commands = new Collection<String, Command>();
+	glob(__dirname + "/commands/**/*", (err, res) => {
+		if (err) {
+			console.log(err)
+		} else {
+			commandFiles = res.filter(file => file.endsWith(
+				process.env.DEVELOPMENT == 'true' ? '.ts' : '.js'
+			));
+			commandFiles = commandFiles.filter((file) => !excludes.some((exclude) => file.includes(exclude)));
+			(async () => {
+				for (const file of commandFiles) {
+					const cmdImp = await import(file)
+					if (Object.keys(cmdImp).length !== 0) {
+						// if(Object.keys(cmdImp)[0].toString() === "default")
+						// 	continue;
+						const cmd: Command = new cmdImp[Object.keys(cmdImp)[0].toString()];
+						commands.set(cmd.data.name, cmd)
+					}
+				}
+			})();
+		}
+	})
+}
 
 // Login to Discord with your client's token
 client.login(process.env.BOT_TOKEN);
